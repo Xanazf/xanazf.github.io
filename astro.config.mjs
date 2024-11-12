4fed33a8226952329396701c88d52ff05cef10da
Xanazf <hotdamnsucka@gmail.com>
2024-11-12 10:41:05 +0200

    glassmorphism

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,14 @@ import { markdownConfig } from "./src/config/io/markdown.ts";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    css: {
+      transformer: "lightningcss",
+    },
+    build: {
+      cssMinify: "lightningcss",
+    },
+  },
   markdown: markdownConfig,
   integrations: [
     solidJs({
